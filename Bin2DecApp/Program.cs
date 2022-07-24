bda036afcfb908d061a78777dcb761d18bfdacfa
Joseph L'Esperance <lesperance.joe@gmail.com>
2022-07-24 08:05:39 -0400

    Add project files.

---
+++ Bin2DecApp/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("--------BINARY TO DECIMAL CONVERTER--------\n");
+Console.Write("Enter a Binary Number: ");
+string binary = Console.ReadLine();
+
+int decimalNum = 0;
+bool binaryIsValid = false;
+
+foreach (char i in binary)
+{
+    if (i == '0' || i == '1')
+    { 
+        binaryIsValid = true; 
+    }
+    else
+    {
+        binaryIsValid = false;
+        break;
+    }
+}
+
+if (binaryIsValid)
+{
+    foreach (char i in binary)
+    {
+        decimalNum = decimalNum * 2 + (i - 48);
+    }
+    Console.WriteLine($"\nDecimal: {decimalNum}\n");
+}
+else
+{
+    Console.WriteLine("\nInvalid input\n");
+}
+
